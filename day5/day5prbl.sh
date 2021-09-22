a773719d9b7ee5ea4e6c91ac45f1fe01dada1754
aravind214 <aravindyelle6@gmail.com>
2021-09-22 17:34:27 +0530

    This is day5 prblms

--- day5/day5prbl.sh
+++ day5/day5prbl.sh
@@ -1,28 +1,51 @@
-# dice1=$(($RANDOM%6))
-# echo $dice1
-# dice2=$(($RANDOM%6))
-# echo $dice2
-# sum=$((dice1+dice2))
-# echo "Sum : $sum"
+echo "1Q. Use Random Function (( RANDOM )) to get Single Digit"
+rand=$(($RANDOM%10))
+echo $rand
 echo "***************************************************"
-
-# for (( i=1; i <= 5; i++ ))
-# do
- # echo "Random number # $i:"$(($RANDOM%20+10))
-# done
+echo "2Q. Use Random to get Dice Number between 1 to 6"
+rand=$(($RANDOM%6+1))
+echo $rand
 echo "***************************************************"
-
-read -p 'Please enter inches:' inches	## User input (int or float)
-
-i2f=0.083333				## 1 Inch = 0.083333 foot 
-
-new_value=$(echo $inches*$i2f ) 	## echo the arithmetic expression to the bc calculator.
+echo "3Q. Add two Random Dice Number and Print the Result"
+ dice1=$(($RANDOM%6))
+ echo $dice1
+ dice2=$(($RANDOM%6))
+ echo $dice2
+ sum=$((dice1+dice2))
+ echo "Sum : $sum"
 echo "***************************************************"
+echo "4Q. Write a program that reads 5 Random 2 Digit values , then find their
+sum and the average"
+randomNum1=$(($RANDOM%100))
+echo $randomNum1
+randomNum2=$(($RANDOM%100))
+echo $randomNum2
+randomNum3=$(($RANDOM%100))
+echo $randomNum3
+randomNum4=$(($RANDOM%100))
+echo $randomNum4
+randomNum5=$(($RANDOM%100))
+echo $randomNum5
+sum=$((randomNum1+randomNum2+randomNum3+randomNum4+randomNum5))
+ echo "Sum : $sum"
+ echo "Average : $(($sum/5))"
+echo "***************************************************"
+echo "5Q. Unit Conversion
+a. 1ft = 12 in then 42 in = ? ft"
+echo "Enter a number in (inch)"
+read -p 'Please enter inches:' inches	
+
+i2f=0.083333				
+
+new_value=$(echo $inches*$i2f ) 	
+echo "***********"
 echo "$inches inches =" $new_value feet 
 
-echo "***************************************************"
+echo "***********"
+echo "5Q. Unit Conversion
+b. Rectangular Plot of 60 feet x 40 feet in meters"
 echo "Enter  length of rectangle :"
 read length
 echo " Enter breadth of rectangle :"
 read breadth
-echo "Area of Rectangle :"$(($length * $breadth))
+echo "Area of Rectangle :"$(($length * $breadth)) * 0.092903
--- day5/day5prblm2.sh
+++ day5/day5prblm2.sh
@@ -1,3 +1,221 @@
+echo "1Q. Write a program that reads 5 Random 3 Digit values and then outputs the minimumand the maximum value ?"
+
+random1=$(((RANDOM%99)+100))
+random2=$(((RANDOM%9)+100))
+random3=$(((RANDOM%99)+100))
+random4=$(((RANDOM%99)+100))
+random5=$(((RANDOM%99)+100))
+echo "Ans :" $random1; echo $random2; echo $random3; echo $random4; echo $random5
+if (( ($random1>$random2) && ($random1>$random3) && ($random1>$random4) && ($random1>$random5) )); then
+        echo "maximum number is $random1"
+elif (( ($random2>$random1) && ($random2>$random3) && ($random2>$random4) && ($random2>$random5) )); then
+        echo "maximum number is $random2"
+elif (( ($random3>$random1) && ($random3>$random2) && ($random3>$random4) && ($random3>$random5) )); then
+        echo "maximum number is $random3"
+elif (( ($random4>$random1) && ($random4>$random3) && ($random4>$random2) && ($random4>$random5) )); then
+        echo "maximum number is $random4"
+else
+        echo "maximum number is $random5"
+fi
+
+#minimun value
+if (( ($random1<$random2) && ($random1<$random3) && ($random1<$random4) && ($random1<$random5) )); then
+        echo "minimum number is $random1"
+elif (( ($random2<$random1) && ($random2<$random3) && ($random2<$random4) && ($random2<$random5) )); then
+        echo "minimum number is $random2"
+elif (( ($random3<$random1) && ($random3<$random2) && ($random3<$random4) && ($random3<$random5) )); then
+        echo "minimum number is $random3"
+elif (( ($random4<$random1) && ($random4<$random3) && ($random4<$random2) && ($random4<$random5) )); then
+        echo "minimum number is $random4"
+else
+        echo "minimum number is $random5"
+fi
+echo "****************************************************************"
+
+echo "2Q. Write a program that takes day and month from the command line and prints true if day of month is between March 20 and June 20, false otherwise...?"
+echo "Ans :"
+read -p " Enter Date:-" date
+read -p " Enter Month:-" Month
+
+if (( ($Month <=6  & $date <= 20) ))
+then
+        echo $Month $date "True";
+
+elif (( ($Month >= 3 & $Month < 6) & ($date<20)  ))
+then
+        echo $date $Month "True";
+else
+        echo "False";
+fi
+echo "******************************************************************"
+
+echo "3Q. Write a program that takes a year as input and outputs the Year is a Leap Year or nota Leap Year. A Leap Year checks for 4 Digit Number, Divisible by 4 and not 100 unless divisible by 400...?"
+echo "Ans :"
+echo "Enter the year (YYYY)"
+read year
+if [ $((year % 4)) -eq 0 ]
+then
+  if [ $((year % 100)) -eq 0 ]
+    then
+    if [ $((year % 400)) -eq 0 ]
+          then
+        echo "its a leap year"
+    else
+           echo "its not a leap year"
+    fi
+  else
+  echo "Its a leap year"
+  fi
+else
+ echo "its not a leap year"
+fi
+echo "******************************************************************"
+echo "4Q. Write a program to simulate a coin flip and print out "Heads" or "Tails" accordingly...?"
+echo "Ans :"
+FLIP=$(($(($RANDOM%10))%2))
+if [ $FLIP -eq 1 ];then
+    echo "heads"
+else
+    echo "tails"
+fi
+
+echo "****************************************************************"
+echo "************************** if-elif-else *****************************"
+
+echo "2Q. Read a Number and Display the week day (Sunday, Monday,...)?"
+
+echo "Ans : ""Enter a number"
+read num
+  if [[ $num == 1 ]]
+  then
+      echo "Monday"
+  elif [[ $num == 2 ]]
+  then
+      echo "Tuesday"
+  elif [[ $num == 3 ]]
+  then
+      echo "Wednesday"
+  elif [[ $num == 4 ]]
+  then
+      echo "Thursday"
+  elif [[ $num == 5 ]]
+  then
+      echo "Friday"
+  elif [[ $num == 6 ]]
+  then
+      echo  "Saturday"
+  elif [[ $num == 7 ]]
+  then
+      echo "Sunday"
+
+  else
+      echo "Invalid day number"
+  fi
+echo "****************************************************************"
+
+echo "3Q. Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...? "
+
+echo "Ans : " "Enter a number"
+read num
+  if [[ $num == 1 ]]
+  then
+      echo "Unit"
+  elif [[ $num == 10 ]]
+  then
+      echo "Ten"
+  elif [[ $num == 100 ]]
+  then
+      echo "Hundred"
+  elif [[ $num == 1000 ]]
+  then
+      echo "Thousand"
+  else
+      echo "Invalid user input number"
+  fi
+echo "****************************************************************"
+
+echo "1Q.  Read a single digit number and write the number in word ?"
+echo "Enter a number"
+read num
+  if [[ $num == 1 ]]
+  then
+      echo "one"
+  elif [[ $num == 2 ]]
+  then
+      echo "Two"
+  elif [[ $num == 3 ]]
+  then
+      echo "Three"
+  elif [[ $num == 4 ]]
+  then
+      echo "Four"
+  elif [[ $num == 5 ]]
+  then
+      echo "Five"
+  elif [[ $num == 6 ]]
+  then
+      echo "Six"
+  elif [[ $num == 7 ]]
+  then
+      echo "Seven"
+  elif [[ $num == 8 ]]
+  then
+      echo "Eight"
+  elif [[ $num == 9 ]]
+  then
+      echo "Nine"
+  else
+      echo "Invalid Single digit input number"
+  fi
+echo "****************************************************************"
+
+echo "4Q.  Enter 3 Numbers do following arithmetic operation and find the one that is maximum and minimum
+1. a + b * c    3. c + a / b
+2. a % b + c   4. a * b + c  "
+
+echo "Enter first number"
+read a
+echo "Enter second number"
+read b
+echo "Enter third number"
+read c
+echo "Compute a+b*c is" $((a+b*c))
+echo "Compute a*b+c is" $((a*b+c))
+echo "Compute c+a/b is" $((c+a/b))
+echo "Compute a%b+c is" $((a%b+c))
+echo "Show above values  : "
+declare -A Arthimaticstore
+Arthimaticstore[a+b*c]=$((a+b*c))
+Arthimaticstore[a*b+c]=$((a*b+c))
+Arthimaticstore[c+a/b]=$((c+a/b))
+Arthimaticstore[a%b+c]=$((a%b+c))
+for key in ${Arthimaticstore[@]};
+do
+echo $key ; 
+done
+echo "this is first "${key[0]}
+echo "this is first "${key[1]}
+echo "this is first "${key[2]}
+echo "this is first "${key[3]}
+
+if (( (${key[0]}<${key[1]}) && (${key[0]}<${key[2]}) && (${key[0]}<${key[3]}) ));
+then
+    echo "minimum value is ${key[0]} "
+elif (( (${key[1]}<${key[0]}) && (${key[1]}<${key[2]}) && (${key[1]}<${key[3]}) ));
+then
+    echo "minimum value is "${key[1]}
+elif (( (${key[2]}<${key[0]}) && (${key[2]}<${key[1]}) && (${key[2]}<${key[3]}) ));
+then
+    echo "minimum value is "${key[2]}
+else 
+   echo "minimum value is "${key[3]}
+fi
+
+echo "****************************************************************"
+echo "************************** case *********************************"
+
+echo "1Q. Read a single digit number and write the number in word using Case ?"
+
 echo -n "Enter number : "
 read n
  
@@ -23,8 +241,11 @@ do
         9) echo -n "nine " ;;
     esac	
 done
-echo""
-echo "************************************************************"
+
+echo "*******************************************************************"
+
+echo "2Q. Read a Number and Display the week day (Sunday, Monday,...)?"
+
 echo "enter a number"
 read n
 case $n in
@@ -37,44 +258,22 @@ case $n in
 7) echo "Saturday" ;;
 *) echo "enter value between 1 to 7" ;;
 esac
-echo""
-echo "************************************************************"
-echo -n "Enter a year:"
-read year_checker
-if [ `expr $year_checker % 4` -eq 0 ]
-then
-	echo "$year_checker is a leap year"
-else
-	echo "$year_checker is not a leap year"
-fi
-echo "************************************************************"
-echo "Enter a number"
-read num
-if[ $num -eq 1 ]
-then
-   echo "one"
-elif[ $num -eq 2 ]
-then
-   echo "Two"
-elif[ $num -eq 3 ]
-then
-   echo "Three"
-elif[ $num == 4 ]
-then
-   echo "Four"
-elif[ $num -eq 5 ]
-then
-   echo "Five"
-elif[ $num -eq 6 ]
-then
-   echo "Six"
-elif[ $num -eq 7 ]
-then
-   echo "Seven"
-elif[ $num -eq 8 ]
-then
-   echo "Eight"
-elif[ $num -eq 9 ]
-then
-   echo "Nine"
-fi
+
+echo "*******************************************************************"
+
+echo "3Q. Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...?"
+
+echo "enter a number"
+read n
+case $n in
+1) echo "Unit" ;;
+10) echo "Ten" ;;
+100) echo "Hundred" ;;
+1000) echo "Thousand" ;;
+10000) echo "Ten Thousand" ;;
+*) echo "Invalid Input" ;;
+esac
+
+echo "*******************************************************************"
+
+
